465fd899f01f9249d8cd5609b199128a1e8b4267
tli533 <tli533@aucklanduni.ac.nz>
2024-06-12 15:39:15 +1000

    Navbar states desktop

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes } from "react-icons/fa"
 
 const NavBar = () => {
+
+
+    const [nav, setNav] = useState(false);
 
     const links = [
         {
@@ -37,17 +40,26 @@ const NavBar = () => {
             <h1 className='text-5xl font-main ml-2'> Test </h1>
         </div>
         
-        <ul className='flex'>
+        <ul className='hidden md:flex'>
 
             {links.map(({id, link}) => (
                 <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>
                     {link}
                 </li>
                 
-            ))}
+            ))} 
+        </ul>
 
+        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-2 z-10 text-gray-500'>
+            {nav ? <FaTimes size ={30} /> : <FaBars size={30} />}
+        </div>
+
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+            <li className='px-4 cursor-pointer capitalize py-6 text-4xl'>home</li>
             
         </ul>
+
+
     </div>
   )
 }
