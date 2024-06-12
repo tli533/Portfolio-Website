7e400a97633f548e8402855931ff5c648192298e
tli533 <tli533@aucklanduni.ac.nz>
2024-06-12 15:21:02 +1000

    NavBar hovers

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import { FaBars, FaTimes } from "react-icons/fa"
 
 const NavBar = () => {
+
+    const links = [
+        {
+            id: 1,
+            link: 'home'
+        },
+        {
+            id: 2,
+            link: 'about'
+        },
+        {
+            id: 3,
+            link: 'portfolio'
+        },
+        {
+            id: 4,
+            link: 'skills'
+        },
+        {
+            id: 5,
+            link: 'experience'
+        },
+        {
+            id: 6,
+            link: 'contact'
+        },
+    ]
+    
+
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black'>
         <div>
@@ -9,8 +38,15 @@ const NavBar = () => {
         </div>
         
         <ul className='flex'>
-            <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>home</li>
-            <li className=''>home</li>
+
+            {links.map(({id, link}) => (
+                <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>
+                    {link}
+                </li>
+                
+            ))}
+
+            
         </ul>
     </div>
   )
