9abe72ee9602db38de6b335899f0433b5d50bec0
tli533 <tli533@aucklanduni.ac.nz>
2024-06-24 23:24:03 +1000

    project template images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import FF2 from "../assets/projects/FF2.png"
+import FF1 from "../assets/projects/FF1 (1).png"
+import FF3FEBRUARY from "../assets/projects/FF3FEBRUARY.png"
+import XI from "../assets/projects/XI.png"
+import XII from "../assets/projects/XII.png"
 
 
 const Projects = () => {
   return (
-    <div>
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
       
       <div>
         <div>
