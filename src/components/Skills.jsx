f22facdc8199bb327d3c173f3ed4869d01d45130
tli533 <tli533@aucklanduni.ac.nz>
2024-06-27 18:18:54 +1000

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import node from "../assets/node.png";
+import react from "../assets/react.png";
+import tailwind from "../assets/tailwind.png";
+import github from "../assets/github.png";
+
 const Skills = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <p>Skills</p>
+            <p>Framworks and tools I've worked with</p>
+          </div>
+
+          <div>
+            <div>
+              <img src={html} alt="" />
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
