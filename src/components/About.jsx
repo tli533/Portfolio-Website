f437e623026443c9c83d5d739fd557abe2d94f13
tli533 <tli533@aucklanduni.ac.nz>
2024-07-02 17:05:58 +1000

    Centering text in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="text-center max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-6xl font-bold inline border-b-4 border-gray-500">
             About
